17021655cff59e95ed58e6b0c9b9cef71da4205f
Nishesh Gautam <mynishesh@gmail.com>
2024-06-26 17:11:41 +0545

    merge with master

---
+++ lib/blocs/calculator_state.dart
@@ -0,0 +1,16 @@
+abstract class CalculaorState{
+
+
+}
+class CalculatorInitialState extends CalculaorState{
+  //starting point of calculator bloc , starting point of bloct
+
+
+}
+
+class CalculatorResult extends CalculaorState{
+  num result;
+  CalculatorResult({required this.result});
+
+
+}
